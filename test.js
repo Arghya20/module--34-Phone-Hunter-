3f419102ee202ae92fcaa3fe07a83cf990034bf1
Arghya20 <arghyaghosh.vlog@gmail.com>
2022-08-30 22:57:06 +0530

    update

--- test.js
+++
@@ -1,7 +0,0 @@
-const phones = [
-  { name: "sony", price: 500 },
-  { name: "apple", price: 700 },
-  { name: "sony", price: 700 },
-];
-
-console.log(phones.filter((phone) => phone.price === 500));
