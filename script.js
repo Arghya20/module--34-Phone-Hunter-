cf8e2abb07654aa80436ab7086c1666e80508490
Arghya20 <arghyaghosh.vlog@gmail.com>
2022-08-30 22:43:09 +0530

    add enter to click and modal popup

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-const loadPhone = async (inputFildValue) => {
+const loadPhone = async (inputFildValue, dataLimit) => {
   const url = `https://openapi.programming-hero.com/api/phones?search=${inputFildValue}`;
   const res = await fetch(url);
   const data = await res.json();
-  phoneData(data.data);
+  phoneData(data.data, dataLimit);
 };
 
-const phoneData = (getPhone) => {
-  getPhone = getPhone.slice(0, 3);
+const phoneData = (getPhone, dataLimit) => {
+  const showMore = document.getElementById("show-more");
+  if (dataLimit && getPhone.length > 5) {
+    getPhone = getPhone.slice(0, 5);
+    showMore.classList.remove("d-none");
+  } else {
+    showMore.classList.add("d-none");
+  }
   const showPhone = document.getElementById("show-phone");
   showPhone.innerHTML = ``;
   showPhone.classList.add("col");
@@ -28,6 +34,7 @@ const phoneData = (getPhone) => {
                             <div class="card-body">
                                 <h5 class="card-title">${element.brand}</h5>
                                 <p class="card-text">${element.phone_name} </p>
+                                <button  data-bs-toggle="modal" data-bs-target="#exampleModal" class="btn btn-info text-white" onclick="loadPhoneDetails('${element.slug}')" >Show Detail</button>
                             </div>
          </div>
         
@@ -37,13 +44,26 @@ const phoneData = (getPhone) => {
   toggleSpinner(false);
 };
 
-document.getElementById("search-btn").addEventListener("click", function () {
+const prossesSearch = (dataLimit) => {
   toggleSpinner(true);
   const inputFild = document.getElementById("input-fild");
   const inputFildValue = inputFild.value;
-  loadPhone(inputFildValue);
-  inputFild.value = "";
+  loadPhone(inputFildValue, dataLimit);
+};
+
+document.getElementById("search-btn").addEventListener("click", function () {
+  prossesSearch(5);
 });
+
+//Enter Button Function ------
+document
+  .getElementById("input-fild")
+  .addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+      // Do work
+      prossesSearch(5);
+    }
+  });
 
 const toggleSpinner = (isLoading) => {
   const spinner = document.getElementById("spanner");
@@ -54,4 +74,26 @@ const toggleSpinner = (isLoading) => {
   }
 };
 
-// loadPhone();
+document.getElementById("btn-show-all").addEventListener("click", function () {
+  prossesSearch();
+});
+
+const loadPhoneDetails = async (id) => {
+  const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  displayDitels(data.data);
+};
+
+const displayDitels = (phoneData) => {
+  console.log(phoneData);
+  const modalTitle = document.getElementById("modal-title");
+  modalTitle.innerText = phoneData.name;
+  const modalDec = document.getElementById("modal-dec");
+  modalDec.innerHTML = `
+  <p>Storage : ${phoneData.mainFeatures.storage}</p>
+  <p>Display Size : ${phoneData.mainFeatures.displaySize}</p>
+  <p> Chipset : ${phoneData.mainFeatures.chipSet}</p>
+  <p> Memory : ${phoneData.mainFeatures.memory}</p>
+  `;
+};
---
+++ test.js
@@ -0,0 +1,7 @@
+const phones = [
+  { name: "sony", price: 500 },
+  { name: "apple", price: 700 },
+  { name: "sony", price: 700 },
+];
+
+console.log(phones.filter((phone) => phone.price === 500));
